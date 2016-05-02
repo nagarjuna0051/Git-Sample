cdf144919697a01cd489b194518f564cd202b116
SkoopView <skoopview@skoopviews-mac-mini.local>
2016-05-02 10:18:58 +0530

    Initial Commit

---
+++ Git Sample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Git Sample
+//
+//  Created by SkoopView on 02/05/16.
+//  Copyright © 2016 SkoopView. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Git Sample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Git Sample
+//
+//  Created by SkoopView on 02/05/16.
+//  Copyright © 2016 SkoopView. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
